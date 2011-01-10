49022d45827eee344b5f33b3b79e727a637cbc87
Brian Strand <brstrand@gmail.com>
2011-01-10 22:45:21 +0000

    should be part of client projects, not android-plugin itself

--- src/main/scala/AndroidDefaults.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-trait AndroidDefaults extends AndroidBase {
-  override def androidPlatformName = "android-7"
-
-  // try to keep all config in the sbt project rather than in random environment vars
-  override lazy val sdkRoot = Path.userHome / "android" / "sdk"
-}
